8dc1be94d3c567a4fa83c587a910cf8a98374023
Ze Dang <4649tz@gmail.com>
2020-08-16 17:55:58 +0800

    [zedang] feat: implement buy

---
+++ src/main/java/com/thoughtworks/rslist/repository/TradeRepository.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.rslist.repository;
+
+import com.thoughtworks.rslist.dto.TradeDto;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface TradeRepository extends CrudRepository<TradeDto, Integer> {
+    @Override
+    List<TradeDto> findAll();
+    List<TradeDto> findAllByRsEventId(int rsEventId);
+}
